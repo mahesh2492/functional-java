6b74a445918745876a8cc095b8ef2cccfdecffc3
mahesh2492 <mahesh.kndpl@gmail.com>
2020-01-16 10:20:43 +0530

    optimized the execution of leaders in an array

--- src/main/java/imperative/LeadersArray.java
+++ src/main/java/imperative/LeadersArray.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class LeadersArray {
@@ -66,27 +67,45 @@ public class LeadersArray {
 
     public static void foo(int []a, int n) {
         boolean flag ;
-        ArrayList<Integer> al = new ArrayList<>() ;
-        al.add(a[n - 1]) ;
-        for(int i = n - 2; i >= 0 ; i--) {
+        StringBuffer sb =new StringBuffer();
+
+        for(int i = 0; i < n - 1 ; i++) {
             flag = false ;
-            for(int j = i + 1; j < n; j++) {
+            for(int j = i + 1; j < n ; j++) {
                 if(a[i] >= a[j]) {
                     flag = true;
                 } else {
-                    flag = false ;
+                    flag = false;
                     break;
                 }
             }
 
             if(flag) {
-                al.add(a[i]) ;
+                sb.append(a[i]) ;
+                sb.append(' ');
+            }
+        }
+        sb.append(a[n - 1]) ;
+        System.out.println(sb);
+
+    }
+
+    public static void printLeaders(int []a, int n) {
+        StringBuilder sb = new StringBuilder() ;
+        Stack<Integer> st = new Stack<>() ;
+        st.push(a[n - 1]) ;
+        for(int i  = n - 2 ; i >= 0 ; i--) {
+            if(a[i] >= st.peek()) {
+                st.push(a[i]) ;
             }
         }
 
-        System.out.println(al);
-        for(int i = al.size() - 1 ; i >= 0 ; i--)
-            System.out.print(al.get(i) + " ");
+        while(!st.isEmpty()) {
+            sb.append(st.pop()) ;
+            sb.append(' ');
+        }
+
+        System.out.println(sb);
     }
 
     public static void main (String[] args) {
@@ -105,6 +124,7 @@ public class LeadersArray {
             foo(a, n);
             System.out.println();
         }
+
     }
 
 }
